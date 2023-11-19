d1845c304f3b903a22e29e20f8ccf00702159fe2
Kelvyn Marte <kelvyn.marte@pwc.com>
2023-11-19 08:43:57 +0100

    Add sanitize.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles/skeleton.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'sanitize.css';
+import './styles/skeleton.css';
+
 import {
-  createHashRouter,
-  RouterProvider,
+	createHashRouter,
+	RouterProvider,
 } from "react-router-dom";
 import Home from './pages/Home.tsx';
 import Diy from './pages/Diy.tsx';
 
+
 const router = createHashRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "/diy",
-    element: <Diy />
-  },
+	{
+		path: "/",
+		element: <Home />
+	},
+	{
+		path: "/diy",
+		element: <Diy />
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
+);
