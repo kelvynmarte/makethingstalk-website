b304b5e0a1f0d36487ca3b06b25de04ae1f44846
Kelvyn Marte <kelvyn.marte@pwc.com>
2023-11-19 23:04:37 +0100

    Adapt rem Content Section Wrapper

--- src/components/ContentSectionWrapper.tsx
+++ src/components/ContentSectionWrapper.tsx
@@ -4,6 +4,6 @@ export const ContentSectionWrapper = styled.div`
     grid-column: full-width;
     display: grid;
     grid-template-columns: inherit;
-    row-gap: clamp(6.45rem, 5.2686rem + 5.907vw, 11.1756rem);
-    margin: clamp(6.45rem, 5.2686rem + 5.907vw, 11.1756rem) 0;
+    row-gap: clamp(4rem, 3rem + 7.907vw, 11.1756rem);
+    margin: clamp(4rem, 3rem + 7.907vw, 11.1756rem) 0;
 `;
